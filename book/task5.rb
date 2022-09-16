8c5a6d9f7ef6cef7378e2fa83390567092253871
E8ro <s8000s@gmail.com>
2022-09-16 10:16:09 +0300

    edit task5

---
+++ book/task5.rb
@@ -0,0 +1,18 @@
+# puts rand(500..510)
+
+# puts rand(0..1.0)
+
+# puts rand(2.0..4)
+
+x = rand(0..10)
+loop do
+    print 'Угадай число от 0 до 10 - '
+    ans = gets.to_i
+
+    if x == ans
+        puts 'Вы угадали!'
+        exit
+    else
+        puts 'Не верно..'
+    end    
+end
